6c354a4719c188b18a4d4e795adc14626ea58d3d
R3X0-NULL <jayden7deley@yandex.ru>
2023-01-26 01:26:02 +0300

    Task 3 complete

---
+++ Lesson 2/Task 2/task 2/task 2/mian.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+struct adress_info {
+	string city_name;
+	string street_name;
+	int house_number;
+	int flat_number;
+	int index;
+};
+
+void struct_cout(struct adress_info& struct_cout) {
+	cout << "City: " << struct_cout.city_name << "\n";
+	cout << "Street: " << struct_cout.street_name << "\n";
+	cout << "House number: " << struct_cout.house_number << "\n";
+	cout << "Flat number: " << struct_cout.flat_number << "\n";
+	cout << "Index: " << struct_cout.index << "\n\n";
+}
+
+int main() {
+	adress_info Moscow{"Moscow", "Arbath", 12, 8, 123456};
+	adress_info Izhevsk{ "Izhevsk","Pushkina", 59, 143, 953769 };
+
+	struct_cout(Moscow);
+	struct_cout(Izhevsk);
+
+	return 0;
+}
