a3a22ff6019949d1281c55ce95b7b7359378dc21
R3X0-NULL <jayden7deley@yandex.ru>
2023-02-20 12:12:00 +0300

    ...

--- Lesson 8/Task 1/Task 1/main.cpp
+++ Lesson 8/Task 1/Task 1/main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-	try {
-		
+int function(string str, int forbiden_lenght) {
+	if (str.length() == forbiden_lenght) {
+		throw exception("Вы ввели слово запретной длины!");
 	}
-	excep
+}
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	string str;
+	int forbiden_lenght;
+
+	cout << "Введите запретную длину: ";
+	cin >> forbiden_lenght;
+
+	while (true) {
+		try {
+			cout << "Введите слово: ";
+			cin >> str;
+			cout << "Длина слова " << str << " равна: " << function(str, forbiden_lenght) << "\n";
+		}
+		catch (exception& ex) {
+			cout << ex.what();
+			break;
+		}
+	}
+
 	return 0;
 }
